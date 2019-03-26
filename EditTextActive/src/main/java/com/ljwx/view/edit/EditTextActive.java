3b9060a5823486a7f77b8136f7bbdf1938d6e160
9527 <1146699396@qq.com>
2019-03-26 19:34:56 +0800

    delete one attribute

--- EditTextActive/src/main/java/com/ljwx/view/edit/EditTextActive.java
+++ EditTextActive/src/main/java/com/ljwx/view/edit/EditTextActive.java
@@ -7,11 +7,14 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.support.v7.widget.AppCompatEditText;
+import android.text.InputFilter;
 import android.util.AttributeSet;
+
+import java.lang.reflect.Field;
 
 public class EditTextActive extends AppCompatEditText {
 
-    private int mNumDigit = 0;
+    private int mMaxLenght = 0;
     private float mMarginValue = 0;
     private float mRadiu = 0f;
     private Paint mBgPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -25,18 +28,22 @@ public class EditTextActive extends AppCompatEditText {
 
     public EditTextActive(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mBgPaint.setColor(mBgColorN);
         TypedArray attr = context.obtainStyledAttributes(attrs, R.styleable.EditTextActive);
         mMarginValue = attr.getDimension(R.styleable.EditTextActive_etaMarginLeft, 30f);
         mRadiu = attr.getDimension(R.styleable.EditTextActive_etaRadiu, 15);
         mBgColorN = attr.getColor(R.styleable.EditTextActive_etaBgColor, mBgColorN);
         mBgColorY = attr.getColor(R.styleable.EditTextActive_etaBgColor, mBgColorY);
-        mNumDigit = attr.getInteger(R.styleable.EditTextActive_etaMaxDigit, 4);
         attr.recycle();
+        init();
     }
 
     public EditTextActive(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    public void init() {
+        mMaxLenght = getMaxLength();
+        setCursorVisible(false);
     }
 
 //    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -55,7 +62,7 @@ public class EditTextActive extends AppCompatEditText {
 //        }
 //
 //        if (MeasureSpec.AT_MOST == widthMode) {
-//            width = getHeight() * mNumDigit + ((mNumDigit - 1) * mMarginValue);
+//            width = getHeight() * mMaxLenght + ((mMaxLenght - 1) * mMarginValue);
 //        }
 //        setMeasuredDimension(width, height);
 //    }
@@ -69,7 +76,7 @@ public class EditTextActive extends AppCompatEditText {
 //    @Override
 //    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
 //        super.onLayout(changed, left, top, right, bottom);
-//        setMeasuredDimension(getHeight() * mNumDigit + ((mNumDigit - 1) * mMarginValue), getHeight());
+//        setMeasuredDimension(getHeight() * mMaxLenght + ((mMaxLenght - 1) * mMarginValue), getHeight());
 //    }
 
     @Override
@@ -84,7 +91,7 @@ public class EditTextActive extends AppCompatEditText {
         RectF rf = new RectF(0, 0, getHeight(), getHeight());
         mBgPaint.setColor(mBgColorN);
         canvas.save();
-        for (int i = 0; i < mNumDigit; i++) {
+        for (int i = 0; i < mMaxLenght; i++) {
             canvas.drawRoundRect(rf, mRadiu, mRadiu, mBgPaint);
             canvas.translate((getHeight() + mMarginValue), 0);
         }
@@ -95,7 +102,7 @@ public class EditTextActive extends AppCompatEditText {
         RectF rf = new RectF(0, 0, getHeight(), getHeight());
         canvas.save();
         mBgPaint.setColor(mBgColorY);
-        for (int i = 0; i < Math.min(getText().length(),mNumDigit); i++) {
+        for (int i = 0; i < Math.min(getText().length(), mMaxLenght); i++) {
             float t = (getHeight() + mMarginValue);
             canvas.drawRoundRect(rf, mRadiu, mRadiu, mBgPaint);
             canvas.translate(t, 0);
@@ -105,7 +112,7 @@ public class EditTextActive extends AppCompatEditText {
 
     private void drawText(Canvas canvas) {
         canvas.save();
-        for (int i = 0; i < Math.min(getText().length(), mNumDigit); i++) {
+        for (int i = 0; i < Math.min(getText().length(), mMaxLenght); i++) {
             String num = getText().charAt(i) + "";
             float x = getHeight() / 2 - getPaint().measureText(num) / 2;
             float y = (getHeight() / 2) + Math.abs(getPaint().ascent() + getPaint().descent()) / 2;
@@ -115,4 +122,26 @@ public class EditTextActive extends AppCompatEditText {
         canvas.restore();
     }
 
+
+    public int getMaxLength() {
+        int length = 4;
+        try {
+            InputFilter[] inputFilters = getFilters();
+            for (InputFilter filter : inputFilters) {
+                Class<?> c = filter.getClass();
+                if (c.getName().equals("android.text.InputFilter$LengthFilter")) {
+                    Field[] f = c.getDeclaredFields();
+                    for (Field field : f) {
+                        if (field.getName().equals("mMax")) {
+                            field.setAccessible(true);
+                            length = (Integer) field.get(filter);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return length;
+    }
 }
